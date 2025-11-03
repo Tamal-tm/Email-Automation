f0600f982acc33051e4193c12549925b82b16618
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-03 08:34:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,6 @@ import pyttsx3
 listener=sr.Recognizer()
 tts=pyttsx3.Engine()
 
-
 def talking_tom(text):
     tts.say(text)
     tts.runAndWait()
@@ -46,5 +45,6 @@ def main_code():
 
 
 main_code()
+
 
 
